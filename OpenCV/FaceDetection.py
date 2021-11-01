2c9e1a73321e3c53c718e0f19936667da3f63b13
mvog2501 <91571536+mvog2501@users.noreply.github.com>
2021-10-31 21:21:13 -0500

    I trained a cascade. Don't know if it works. Negative images are going into "Maybe" too.

--- OpenCV/FaceDetection.py
+++ OpenCV/FaceDetection.py
@@ -1,14 +1,16 @@
 import cv2
 
-faceCascade = cv2.CascadeClassifier("OpenCV/resources/haarcascade_frontalface_default.xml")
-img = cv2.imread("OpenCv/resources/Lenna.png")
+faceCascade = cv2.CascadeClassifier("OpenCV/Cascades/haarcascade_ball_start.xml")
+img = cv2.imread("OpenCv/resources/Image_Pos_4.png")
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 cv2.imshow("Gray",imgGray)
 faces = faceCascade.detectMultiScale(imgGray,1.1,4)
 
+
 for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    cv2.rectangle(img,(x+15,y+15),(x+w-15,y+h-15),(255,0,0),2)
+    print("Ball found")
 
 cv2.imshow("Result", img)
 cv2.waitKey(0)
