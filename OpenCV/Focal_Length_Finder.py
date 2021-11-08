591a840c85af153537dbb336c9fcc9b74ef8424c
mvog2501 <91571536+mvog2501@users.noreply.github.com>
2021-11-08 15:00:13 -0600

    Added a way to find rectangle corners.

--- OpenCV/Focal_Length_Finder.py
+++ OpenCV/Focal_Length_Finder.py
@@ -11,19 +11,43 @@ print("Distance: " + distance + " in")
 
 #Define path
 path = "OpenCV/resources/Rectangle.jpg"
-image = cv2.imread(path)
 
 
-color = (255,0,0)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray,(5,5),0)
-canny = cv2.Canny(gray,30,150)
-contours, hierarchy = cv2.findContours(canny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-cv2.imshow("Canny", blurred)
 
 
-def getPixelsToDistance(objectWidth,objectHeight,image,distance):
+def getRectangle(path):
+    image = cv2.imread(path)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray,(5,5),0)
+    canny = cv2.Canny(gray,30,150)
+    contours, hierarchy = cv2.findContours(canny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    cv2.imshow("Canny", blurred)
+    color = (255,0,0)
+
+    if len(contours)>1:
+        bestContour = max(contours, key = cv2.contourArea)
+
+        minRect = cv2.minAreaRect(bestContour)
+        box = cv2.boxPoints(minRect)
+        box = np.int0(box)
+        cv2.drawContours(image, [box], 0, color)
+
+        x_1 = box[0]
+        x_2 = box[3]
+        y_1 = box[1]
+        y_2 = box[2]
+
+        print("We got 'em")
+        print(x_2)
+
+    else: print("Hmmm")
+    
+    return(x_1[0],x_2[0])
+
+
+def getPixelsToDistance(objectWidth,objectHeight,x1,x2,y1,y2,distance):
     
     #Pixels to inches
     wPix = math.sqrt(math.pow(x_2-x_1,2) + math.pow(y_2-y_1,2))
@@ -33,18 +57,13 @@ def getPixelsToDistance(objectWidth,objectHeight,image,distance):
     
     return horFOV
 
-if len(contours)>1:
-    bestContour = max(contours, key = cv2.contourArea)
-
-    minRect = cv2.minAreaRect(bestContour)
-    box = cv2.boxPoints(minRect)
-    box = np.int0(box)
-    cv2.drawContours(image, [box], 0, color)
-
-else: print("Hmmm")
 
 
 
 
-cv2.imshow("With boxes",image)
+x1,x2 = getRectangle(path)
+
+print(x2)
+
+
 cv2.waitKey(0)
