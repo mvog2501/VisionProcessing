b87f7d056f266d5738de483e9a5bb8980cd261ed
mvog2501 <91571536+mvog2501@users.noreply.github.com>
2021-11-09 20:54:50 -0600

    Angle now represents value within horizontalFOV
    Just did a bit of multiplying by a fraction.

--- OpenCV/Vision_Processing.py
+++ OpenCV/Vision_Processing.py
@@ -76,7 +76,8 @@ class Vision:
 
             distance = (.5 * frameInches)/math.tan(.5*horFOV)
 
-            angleToBall = math.degrees(math.atan2(-(.5*frame.shape[1]-(x+.5*w)),distance))
+            angle = math.degrees(math.atan2(-(.5*frame.shape[1]-(x+.5*w)),distance))
+            angleToBall = angle * (horFOV / 90)
 
         cv2.imshow("Result",frameResult)
         cv2.imshow("Binary",binary)
